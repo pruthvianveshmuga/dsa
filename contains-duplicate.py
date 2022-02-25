f3970baaeac43a02fa51ada262fc646e85db6442
Pruthvi Muga <pruthvianvesh@gmail.com>
2022-02-25 13:42:34 +0530

    sorting

---
+++ contains-duplicate.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+import unittest
+
+# sorting - O(nlogn)
+def solution1(nums):
+    sorted = nums[:]
+    sorted.sort()
+    for i in range(len(sorted)-1):
+        if sorted[i] == sorted[i+1]:
+            return True
+    return False
+
+
+class Tests(unittest.TestCase):
+    cases = [
+        {'inp': [1,2,3,1], 'out': True},
+        {'inp': [1,2,3,4], 'out': False},
+        {'inp': [1,1,1,3,3,4,3,2,4,2], 'out': True},
+    ]
+    solutions = [solution1]
+    def test(self):
+        for sol in self.solutions:
+            for case in self.cases:
+                self.assertEqual(sol(case['inp']), case['out'])
+
+if __name__ == '__main__':
+    unittest.main()
