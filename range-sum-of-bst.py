71d339b36b38e15c3290c5e72cd87d4ea5892358
Pruthvi Muga <pruthvianvesh@gmail.com>
2022-02-25 13:18:06 +0530

    dfs with stack

--- range-sum-of-bst.py
+++ range-sum-of-bst.py
@@ -8,6 +8,24 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# DFS with stack
+def solution2(root: TreeNode, low, high):
+    stack = [root]
+    ans = 0
+    while stack:
+        node = stack.pop()
+        if not node:
+            continue
+        if node.val < low:
+            stack.append(node.right)
+        elif node.val > high:
+            stack.append(node.left)
+        else:
+            ans += node.val
+            stack.append(node.right)
+            stack.append(node.left)
+    return ans
+
 # DFS - time O(n), space O(n) where n is the number of nodes in the tree
 def solution1(root: TreeNode, low, high):
     ans = 0
