88f2a1bfdcd5399d06f878885ff53123059331f9
Pruthvi Muga <pruthvianvesh@gmail.com>
2022-02-26 09:07:41 +0530

    bfs

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,6 +1,30 @@
 # https://leetcode.com/problems/number-of-islands/
 
+from collections import deque
 import unittest
+
+def bfs(grid, row, col):
+    neighbours = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    q = deque()
+    grid[row][col] = '0'
+    q.append((row, col))
+    while q:
+        i, j = q.popleft()
+        for neighbour in neighbours:
+            newI, newJ = i + neighbour[0], j + neighbour[1]
+            if (0 <= newI < len(grid)) and (0 <= newJ < len(grid[newI])) and grid[newI][newJ] == '1':
+                grid[newI][newJ] = '0'
+                q.append((newI, newJ))
+
+# bfs
+def solution2(grid):
+    islands = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == '1':
+                islands += 1
+                bfs(grid, row, col)
+    return islands
 
 
 def dfs(grid, visited, row, col):
@@ -11,7 +35,7 @@ def dfs(grid, visited, row, col):
     for neighbour in neighbours:
         dfs(grid, visited, row + neighbour[0], col + neighbour[1])
     
-
+# dfs
 def solution1(grid):
     visited = [[False for el in row] for row in grid]
     islands = 0
@@ -44,7 +68,7 @@ class Tests(unittest.TestCase):
             'out': 3
         },
     ]
-    solutions = [solution1]
+    solutions = [solution1, solution2]
     def test(self):
         for sol in self.solutions:
             for case in self.cases:
