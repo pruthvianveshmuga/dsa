9a79bb4a745b9aeb8431266fb1410f738cc428d9
Pruthvi Anvesh Muga <39563717+pruthvianveshmuga@users.noreply.github.com>
2022-03-09 19:54:42 +0530

    Count binary substrings (#23)
    * on the fly

--- count-binary-substrings.py
+++ count-binary-substrings.py
@@ -2,6 +2,20 @@
 
 import unittest
 
+# O(n) time and O(1) space - on the fly
+def solution2(s):
+    prev, curr, ans = 0, 1, 0
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            curr += 1
+        else:
+            ans += min(curr, prev)
+            prev, curr = curr, 1
+    else:
+        ans += min(prev, curr)
+    return ans
+
+
 # O(n) - grouping
 def solution1(s):
     groups = [1]
@@ -21,7 +35,7 @@ class Tests(unittest.TestCase):
         {'inp': '00110011', 'out': 6},
         {'inp': '10101', 'out': 4},
     ]
-    solutions = [solution1]
+    solutions = [solution1, solution2]
     def test(self):
         for sol in self.solutions:
             for case in self.cases:
