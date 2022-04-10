060f1c3b292327618be3cd2df86f55ee64680ea3
Pruthvi Anvesh Muga <39563717+pruthvianveshmuga@users.noreply.github.com>
2022-04-10 23:49:50 +0530

    next permutation (#40)

---
+++ next-permutation.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/next-permutation/
+
+import unittest
+
+# O(n)
+def solution1(nums):
+    for i in reversed(range(len(nums)-1)):
+        if nums[i] < nums[i+1]:
+            break
+    else:
+        nums.sort()
+        return nums
+    for j in reversed(range(i+1, len(nums))):
+        if nums[j] > nums[i]:
+            nums[i], nums[j] = nums[j], nums[i]
+            break
+    nums[i+1:] = sorted(nums[i+1:])
+    return nums
+
+class Tests(unittest.TestCase):
+    cases = [
+        {'inp': [4,3,2,1], 'out': [1,2,3,4]},
+        {'inp': [1,4,3,2], 'out': [2,1,3,4]},
+        {'inp': [2,1,5,4,3], 'out': [2,3,1,4,5]},
+        {'inp': [1,2,5,4,3], 'out': [1,3,2,4,5]},
+        {'inp': [1,2,3], 'out': [1,3,2]},
+        {'inp': [3,2,1], 'out': [1,2,3]},
+        {'inp': [1,1,5], 'out': [1,5,1]},
+    ]
+    solutions = [solution1]
+    def test(self):
+        for sol in self.solutions:
+            for case in self.cases:
+                self.assertEqual(sol(case['inp']), case['out'])
+
+if __name__ == '__main__':
+    unittest.main()
