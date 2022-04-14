90da601c342daec274e51a82b9bb4d7cf1077346
Pruthvi Anvesh Muga <39563717+pruthvianveshmuga@users.noreply.github.com>
2022-04-14 18:32:48 +0530

    3sum hash (#43)
    * brute
    
    * hashing

--- 3sum.py
+++ 3sum.py
@@ -2,6 +2,19 @@
 
 import unittest
 
+# O(n^2) - hashing
+def solution2(nums):
+    n = len(nums)
+    ans_set = set()
+    numToInd = { num:i for i, num in enumerate(nums)}
+    for i in range(n):
+        for j in range(i+1, n):
+            val = -(nums[i]+nums[j])
+            if val in numToInd and numToInd[val] not in [i, j]:
+                ans_set.add(tuple(sorted((nums[i], nums[j], val))))
+    return [list(x) for x in ans_set]
+
+
 # O(n^3) - brute - TLE
 def solution1(nums):
     n = len(nums)
@@ -15,11 +28,11 @@ def solution1(nums):
 
 class Tests(unittest.TestCase):
     cases = [
-        {'inp': [-1,0,1,2,-1,-4], 'out': [[-1,-1,2],[-1,0,1]]},
+        {'inp': [-1,0,1,2,-1,-4], 'out': [[-1,0,1],[-1,-1,2]]},
         {'inp': [], 'out': []},
         {'inp': [0], 'out': []},
     ]
-    solutions = [solution1]
+    solutions = [solution1,solution2]
     def test(self):
         for sol in self.solutions:
             for case in self.cases:
