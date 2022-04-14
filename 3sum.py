f59a1b67173cd1bc475475d619cf5209154e6ea5
Pruthvi Anvesh Muga <39563717+pruthvianveshmuga@users.noreply.github.com>
2022-04-15 04:18:34 +0530

    3sum two pointer (#44)
    * brute
    
    * hashing
    
    * two pointer
    
    * test todo

--- 3sum.py
+++ 3sum.py
@@ -1,6 +1,37 @@
 # https://leetcode.com/problems/3sum/
 
+from typing import List
 import unittest
+
+def twoSum(arr, reqSum, ans: List):
+    left, right = 0, len(arr)-1
+    while(left < right):
+        sum = arr[left] + arr[right]
+        if sum < reqSum or (left > 0 and arr[left] == arr[left-1]):
+            left += 1
+        elif sum > reqSum or (right < len(arr)-1 and arr[right] == arr[right+1]):
+            right -= 1
+        else:
+            ans.append([-reqSum, arr[left], arr[right]])
+            left += 1
+            right -= 1
+    return
+
+
+# O(n^2) - two pointer
+def solution3(nums):
+    nums.sort()
+    n = len(nums)
+    ans = []
+    for i in range(n):
+        if nums[i] > 0:
+            break
+        elif i > 0 and nums[i] == nums[i-1]:
+            continue
+        else:
+            twoSum(nums[i+1:], -nums[i], ans)
+    return ans
+
 
 # O(n^2) - hashing
 def solution2(nums):
@@ -26,13 +57,15 @@ def solution1(nums):
                     ans_set.add(tuple(sorted((nums[i], nums[j], nums[k]))))
     return [list(x) for x in ans_set]
 
+
+# Note: Tests might fail as the order of list elements might not match. Need a testing func.
 class Tests(unittest.TestCase):
     cases = [
         {'inp': [-1,0,1,2,-1,-4], 'out': [[-1,0,1],[-1,-1,2]]},
         {'inp': [], 'out': []},
         {'inp': [0], 'out': []},
     ]
-    solutions = [solution1,solution2]
+    solutions = [solution1, solution2, solution3]
     def test(self):
         for sol in self.solutions:
             for case in self.cases:
