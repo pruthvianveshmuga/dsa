4df3c0ed882b3fc870bfbacb4c1eb287a4208d1c
Pruthvi Anvesh Muga <39563717+pruthvianveshmuga@users.noreply.github.com>
2022-05-09 10:22:09 +0530

    Longest palindromic substring - from center (#47)
    * brute n^3
    
    * expand Around Center

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -2,6 +2,29 @@
 
 import unittest
 
+def expandAroundCenter(s, left, right):
+    maxPalindrome = ''
+    while left >= 0 and right < len(s):
+        if s[left] != s[right]:
+            break
+        curr = s[left:right+1]
+        maxPalindrome = curr if len(curr) > len(maxPalindrome) else maxPalindrome
+        left, right = left - 1, right + 1
+    return maxPalindrome
+
+# O(n^2) -> expand around the center
+def solution2(s):
+    maxPalindrome = ''
+    for center in range(len(s)):
+        curr = expandAroundCenter(s, center, center)
+        if len(curr) > len(maxPalindrome):
+            maxPalindrome = curr
+        curr = expandAroundCenter(s, center, center + 1)
+        if len(curr) > len(maxPalindrome):
+            maxPalindrome = curr
+    return maxPalindrome
+
+
 def isPalindrome(s):
     left, right = 0, len(s)-1
     while left < right:
@@ -25,7 +48,7 @@ class Tests(unittest.TestCase):
         {'inp': 'babad', 'out': 'bab'},
         {'inp': 'cbbd', 'out': 'bb'},
     ]
-    solutions = [solution1]
+    solutions = [solution1, solution2]
     def test(self):
         for sol in self.solutions:
             for case in self.cases:
